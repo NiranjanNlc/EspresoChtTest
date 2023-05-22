88dc26d10e2d01b3a3f7f798224f1e22e286ee82
niranjannlc <77341018@nou.edu.np>
2023-05-22 14:55:00 +0545

    👨‍🔧 fixed the green part

--- app/src/main/java/org/lniranjan/chatwithespreso/modal/Message.kt
+++ app/src/main/java/org/lniranjan/chatwithespreso/modal/Message.kt
@@ -1,8 +1,12 @@
 package org.lniranjan.chatwithespreso.modal
 
+import kotlin.random.Random
+
 
 // data class mssage containg message and sender and timestamp
 data class Message(
+    //generate the random id of message
+    val id: Int = Random.nextInt(),
     val message: String? =" Hello Niranjan Babby ",
     val sender: String? = "Niranjan",
     val timestamp: String = "10: 58 pm , `12/12/2020`"
--- app/src/main/java/org/lniranjan/chatwithespreso/ui/viewmodal/ConversationViewModal.kt
+++ app/src/main/java/org/lniranjan/chatwithespreso/ui/viewmodal/ConversationViewModal.kt
@@ -1,4 +1,23 @@
 package org.lniranjan.chatwithespreso.ui.viewmodal
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import org.lniranjan.chatwithespreso.modal.Message
+
 class ConversationViewModal {
+
+    val _messageList = MutableLiveData<List<Message>>()
+    val messageList: LiveData<List<Message>>
+        get() = _messageList
+
+    fun sendMessage(message: String) {
+        val newMessage = Message(message = message)
+        val currentMessages = _messageList.value.orEmpty().toMutableList()
+        currentMessages.add(newMessage)
+        _messageList.value = currentMessages
+    }
+
+    fun deleteAllMessages() {
+    _messageList.postValue(emptyList())
+    }
 }
