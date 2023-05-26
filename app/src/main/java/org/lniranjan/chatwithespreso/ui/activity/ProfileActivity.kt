4bff02437da2a4a3ce644f929335434c0e625c88
niranjannlc <77341018@nou.edu.np>
2023-05-26 14:43:36 +0545

    greenary for the button interaction

--- app/src/main/java/org/lniranjan/chatwithespreso/ui/activity/ProfileActivity.kt
+++ app/src/main/java/org/lniranjan/chatwithespreso/ui/activity/ProfileActivity.kt
@@ -2,6 +2,7 @@ package org.lniranjan.chatwithespreso.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import org.lniranjan.chatwithespreso.R
 import org.lniranjan.chatwithespreso.databinding.ActivityProfileBinding
@@ -9,9 +10,31 @@ import org.lniranjan.chatwithespreso.ui.viewmodal.ProfileViewModal
 
 class ProfileActivity : AppCompatActivity() {
     lateinit var binding: ActivityProfileBinding
-    private lateinit var viewModal: ProfileViewModal
+    lateinit var viewModal: ProfileViewModal
+    var isEditMode: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+        viewModal = ProfileViewModal()
+        setEditMode()
+        binding.updateSettingsBtn.setOnClickListener {
+            toggleEditMode()
+        }
+    }
+
+    private fun toggleEditMode() {
+        isEditMode = !isEditMode
+        if (isEditMode) {
+            binding.updateSettingsBtn.text = "Save Profile"
+            setEditMode()
+        } else {
+            binding.updateSettingsBtn.text = "Edit Profile"
+            setEditMode()
+        }
+    }
+
+    private fun setEditMode() {
+        binding.setUserName.isEnabled = isEditMode
+        binding.setBio.isEnabled = isEditMode
     }
 }
